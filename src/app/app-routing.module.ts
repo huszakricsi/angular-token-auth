577fb011d9305c350c5a022fb334570af545ba94
Huszák Richárd János <huszakricsi@gmail.com>
2018-10-06 11:39:54 +0200

    Home Component is added to route's and set as default.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {//home component is the default route
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {//and added to root the home path to HomeComponent
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
