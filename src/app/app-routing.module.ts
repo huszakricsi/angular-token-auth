ca73c571c9005bee05427a96f11652882670fcda
Huszák Richárd János <huszakricsi@gmail.com>
2018-10-07 12:04:18 +0200

    Added route for profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
   {//home component is the default route
@@ -11,6 +12,10 @@ const routes: Routes = [
   {//and added to root the home path to HomeComponent
     path: 'home',
     component: HomeComponent
+  },
+  {//added the profile path to profile component
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
