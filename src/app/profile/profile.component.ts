5ac0dfc318c50529e0633efa13e1b496b624153a
Huszák Richárd János <huszakricsi@gmail.com>
2018-10-08 13:39:40 +0200

    Now the user can change his password.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Angular2TokenService } from 'angular2-token';
+import { Angular2TokenService, UpdatePasswordData } from 'angular2-token';
 import {Http, Response, RequestOptions} from '@angular/http';
+import { MatDialog } from '@angular/material/dialog';
+import { MDialogComponent } from '../m-dialog/m-dialog.component';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +12,28 @@ import {Http, Response, RequestOptions} from '@angular/http';
 export class ProfileComponent implements OnInit {
 
   userdata:any={};
+  updatePasswordData: UpdatePasswordData = <UpdatePasswordData>{};
 
-  constructor(public angular2TokenService:Angular2TokenService,) {
+  constructor(public angular2TokenService:Angular2TokenService, public dialog: MatDialog) {
+    this.updatePasswordData.passwordCurrent='';
+    this.updatePasswordData.password='';
+    this.updatePasswordData.passwordConfirmation='';
   }
 
   ngOnInit() {
+  }
+
+  changePassword(): void{
+    this.angular2TokenService.updatePassword(this.updatePasswordData).subscribe(
+      res => {
+        console.log(res);
+        this.dialog.open(MDialogComponent,{data: "Password changed succesfully!"});
+        this.updatePasswordData.passwordCurrent='';
+        this.updatePasswordData.password='';
+        this.updatePasswordData.passwordConfirmation='';
+      }, error => {
+        this.dialog.open(MDialogComponent,{data: JSON.parse(error._body).errors.full_messages});
+      }
+    );
   }
 }
