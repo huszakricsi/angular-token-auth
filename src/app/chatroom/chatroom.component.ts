c4f6ff653990a87ef203d08eccddd17491a3c3d8
Huszák Richárd János <huszakricsi@gmail.com>
2018-10-13 15:14:56 +0200

    Improved chat experience with user-to-user chat option.

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -16,23 +16,58 @@ export class ChatroomComponent implements OnInit {
 
   conversation_channel: Channel;
   
+  conv_title = "";
+
   conversation:any;
+
+  user_to_user:any;
 
   current_message="";
 
   messages = [];
 
-  users = [];
+  users_in_room = [];
+
+  users= [];
 
   conversations = [];
 
   constructor(public angular2TokenService:Angular2TokenService, private http: HttpClient, private cableService: ActionCableService, public dialog: MatDialog) { }
 
   ngOnInit() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'access-token': this.angular2TokenService.currentAuthData.accessToken,
+        'expiry': this.angular2TokenService.currentAuthData.expiry,
+        'token-type': this.angular2TokenService.currentAuthData.tokenType,
+        'uid': this.angular2TokenService.currentAuthData.uid,
+        'client': this.angular2TokenService.currentAuthData.client
+      })
+    };
+    this.http.get("http://localhost:3000/users",httpOptions).subscribe(
+      res =>{
+        this.users=res['users'];
+      },
+      err =>{
+        console.log(err);
+      }
+    );
     this.overview_channel = this.cableService.cable("ws://localhost:3000/cable?access-token="+this.angular2TokenService.currentAuthData.accessToken+"&client="+this.angular2TokenService.currentAuthData.client+"&uid="+this.angular2TokenService.currentAuthData.uid).channel('ChatroomsChannel');
     this.overview_channel.received().subscribe(res=>{
       console.log(res);
-      this.conversations=res['YourRooms'];
+      let chatrooms_pub=[];
+      let chatrooms_priv=[];
+      for (let entry of res['YourRooms']) {
+          if(entry.identifier.includes(":")===true){
+            chatrooms_priv.push(entry);
+          }
+          else{
+            chatrooms_pub.push(entry);
+          }
+        }
+      console.log(res);
+      this.user_to_user=chatrooms_priv;
+      this.conversations=chatrooms_pub;
     });
   }
   CreateChatroom():void{
@@ -61,14 +96,57 @@ export class ChatroomComponent implements OnInit {
       }
     );
   }
-  Open(conversation):void{
-    this.conversation=conversation;
-    this.conversation_channel = this.cableService.cable("ws://localhost:3000/cable?access-token="+this.angular2TokenService.currentAuthData.accessToken+"&client="+this.angular2TokenService.currentAuthData.client+"&uid="+this.angular2TokenService.currentAuthData.uid).channel('MessagesChannel',{conversation: conversation});
+  OpenUserConversation(user):void{
+    if(this.conversation_channel!=null){
+      this.conversation_channel.unsubscribe();
+      this.conversation=null;
+    }
+    this.conversation_channel = this.cableService.cable("ws://localhost:3000/cable?access-token="+this.angular2TokenService.currentAuthData.accessToken+"&client="+this.angular2TokenService.currentAuthData.client+"&uid="+this.angular2TokenService.currentAuthData.uid).channel('MessagesChannel',{type:"userchat",user: user});
     this.conversation_channel.received().subscribe(res=>{
       console.log(res);
       this.messages=res['messages'];
-      this.users=res['users'];
+      this.users_in_room=res['users'];
+      this.conversation=this.finduserconversationbyuser(user);
+      this.conv_title=this.getUserNameById(user.id);
     });
+  }  
+  finduserconversationbyuser(user):any{
+    let identifier="";
+    if (user["id"]<this.angular2TokenService.currentUserData.id) {
+      identifier=user["id"]+":"+this.angular2TokenService.currentUserData.id;
+    }
+    else {
+      identifier=this.angular2TokenService.currentUserData.id+":"+user["id"];
+    }
+    for (let entry of this.user_to_user) {
+      if(entry.identifier===identifier){
+        return entry;
+      }
+    }
+  }
+  Open(conversation):void{
+    if(this.conversation_channel!=null){
+      this.conversation_channel.unsubscribe();
+    }
+    this.conv_title=conversation.identifier;
+    this.conversation=conversation;
+    this.conversation_channel = this.cableService.cable("ws://localhost:3000/cable?access-token="+this.angular2TokenService.currentAuthData.accessToken+"&client="+this.angular2TokenService.currentAuthData.client+"&uid="+this.angular2TokenService.currentAuthData.uid).channel('MessagesChannel',{type:"chatroom",conversation: conversation});
+    this.conversation_channel.received().subscribe(res=>{
+      console.log(res);
+      this.messages=res['messages'];
+      this.users_in_room=res['users'];
+    });
+  }  
+  Delete(conversation):void{
+    if(this.conversation!=null&&this.conversation_channel!=null&&this.conversation.id==conversation.id){
+      this.conversation_channel.unsubscribe();
+      this.conversation_channel=null;
+      this.conversation=null;
+    }
+    this.overview_channel.perform("delete_Room",{chatroom_id: conversation.id});
+  }  
+  DeleteMessage(message):void{
+    this.conversation_channel.perform("delete_Message",{message_id: message.id, conversation: this.conversation});
   }  
   onKeyUp(event:any){
     if(event.keyCode == 13){
@@ -82,13 +160,16 @@ export class ChatroomComponent implements OnInit {
   }
   getUserNameById(id:number):string{
     let ret="";
-    for (let entry of this.users) {
+    for (let entry of this.users_in_room) {
         if (entry.id==id){
-          if(entry.name!=""){
+          if(entry.name!=""&&entry.name!=null){
             ret = entry.name;
           }
           else{
             ret = entry.uid;
+          }
+          if(entry.nickname!=""&&entry.nickname!=null){
+            ret+= " ("+entry.nickname+")";
           }
         }
     }
